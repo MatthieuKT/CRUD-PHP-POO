a8eb95cc5dd0a0cee90e6facf1c6474ab62a8431
Douze <kwant.matthieu@gmail.com>
2018-03-04 16:00:36 +0100

    ajout de la page create_product.php

---
+++ php-oop-crud-level-1/create_product.php
@@ -0,0 +1,15 @@
+<?php
+// set page headers
+$page_title = "Create Product";
+include_once 'layout_header.php';
+
+echo '<div class="right-button-margin">"';
+  echo '<a href="index.php" class="btn btn-default pull-right">Read Products</a>';
+echo "</div>";
+ ?>
+
+<!-- 'create product' html form will be here -->
+
+<?php// footer
+include_once "layout_footer.php";
+?>
